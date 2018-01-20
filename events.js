3333bd73b92029e25f5e6aaff6e2f5035818cfe0
Vladislav Volivec'kiy <Vladislav.Voliveckiy@gmail.com>
2018-01-20 19:47:35 +0200

    Setting indent by 2 spaces

--- events.js
+++ events.js
@@ -1,43 +1,41 @@
- 
-function addEvent(element, eventName, callback) {
-    if (element.addEventListener) {
-        element.addEventListener(eventName, callback, false);
-    } else if (element.attachEvent) {
-        element.attachEvent("on" + eventName, callback);
-    } else {
-    }
-}
+ function addEvent(element, eventName, callback) {
+   if (element.addEventListener) {
+     element.addEventListener(eventName, callback, false);
+   } else if (element.attachEvent) {
+     element.attachEvent("on" + eventName, callback);
+   }
+ }
 
-function keyPressed(event) {
-    event = event || window.event;
-    var keyCode = event.keyCode || event.charCode;
+ function keyPressed(event) {
+   event = event || window.event;
+   var keyCode = event.keyCode || event.charCode;
 
-    try {
-        if(keyCode >= (48 + 1) && keyCode <= 58) {
-            sudoku_table.updateCell(event.target, keyCode - 48);
-        }
-    } finally {
-        if (event.preventDefault) {
-            event.preventDefault();
-        } else {
-            event.returnValue = false
-        }
-    }
-}
+   try {
+     if (keyCode >= (48 + 1) && keyCode <= 58) {
+       sudoku_table.updateCell(event.target, keyCode - 48);
+     }
+   } finally {
+     if (event.preventDefault) {
+       event.preventDefault();
+     } else {
+       event.returnValue = false;
+     }
+   }
+ }
 
-function keyDown(event) {
-    event = event || window.event;
-    var key = event.keyCode || event.charCode;
+ function keyDown(event) {
+   event = event || window.event;
+   var key = event.keyCode || event.charCode;
 
-    if(key == 8 || key == 46 ) {
-        try {
-            sudoku_table.updateCell(event.target, 0);
-        } finally {
-            if (event.preventDefault) {
-                event.preventDefault();
-            } else {
-                event.returnValue = false
-            }
-        }
-    }
-}
+   if (key == 8 || key == 46) {
+     try {
+       sudoku_table.updateCell(event.target, 0);
+     } finally {
+       if (event.preventDefault) {
+         event.preventDefault();
+       } else {
+         event.returnValue = false;
+       }
+     }
+   }
+ }
--- util.js
+++ util.js
@@ -1,69 +1,74 @@
 function randInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
- 
+
 function getRandBlock() {
-    var array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    var swapCount = randInteger(20, 50);
-    for(var i = 0; i < swapCount; i++) {
-        var swap = function(i, j) {
-            var tmp = array[i];
-            array[i] = array[j];
-            array[j] = tmp;
-        };
-        swap(randInteger(0, 8), randInteger(0, 8));
-    }
-    return array;
+  var array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var swapCount = randInteger(20, 50);
+  for (var i = 0; i < swapCount; i++) {
+    var swap = function (i, j) {
+      var tmp = array[i];
+      array[i] = array[j];
+      array[j] = tmp;
+    };
+    swap(randInteger(0, 8), randInteger(0, 8));
+  }
+  return array;
 }
 
 function isContains(array, value) {
-    return array.indexOf(value) != -1;
+  return array.indexOf(value) != -1;
 }
 
 function inverseSudokuArray(array) {
-    var result = [];
-    for(var i = 1; i <= 9; i++) {
-        if(array.indexOf(i) == -1) {
-            result.push(i);
-        }
+  var result = [];
+  for (var i = 1; i <= 9; i++) {
+    if (array.indexOf(i) == -1) {
+      result.push(i);
     }
-    return result;
+  }
+  return result;
 }
 
 function SudokuArrayIntersection(arr1, arr2, arr3) {
-    var result = [];
-    for(var i = 0; i <= 9; i++) {
-        if(arr1.indexOf(i) != -1 &&
-            arr2.indexOf(i) != -1 &&
-            arr3.indexOf(i) != -1) {
+  var result = [];
+  for (var i = 0; i <= 9; i++) {
+    if (arr1.indexOf(i) != -1 &&
+      arr2.indexOf(i) != -1 &&
+      arr3.indexOf(i) != -1) {
 
-            result.push(i);
-        }
+      result.push(i);
     }
-    return result;
+  }
+  return result;
 }
 
 var FIELD_WIDTH = 9;
 
 function matrixCoordFromAbs(abs) {
-    return {x: Math.floor(abs / FIELD_WIDTH),
-            y: abs % FIELD_WIDTH};
+  return {
+    x: Math.floor(abs / FIELD_WIDTH),
+    y: abs % FIELD_WIDTH
+  };
 }
 
 function segmentPosFromIndex(index) {
-    var row = index - index % 3;
-    var col = (index % 3) * 3;
-    return {x : row, y: col};
+  var row = index - index % 3;
+  var col = (index % 3) * 3;
+  return {
+    x: row,
+    y: col
+  };
 }
 
 function fillSudokuArray(array, bottom, top, left, right, fillValue) {
-    var resultArray;
-    for(var i = top; i < bottom; i++) {
-        for(var j = left; j < right; j++) {
-            array[i * 9 + j] = fillValue;
-        }
+  var resultArray;
+  for (var i = top; i < bottom; i++) {
+    for (var j = left; j < right; j++) {
+      array[i * 9 + j] = fillValue;
     }
-    return resultArray;
+  }
+  return resultArray;
 }
 
 /**
@@ -72,14 +77,14 @@ function fillSudokuArray(array, bottom, top, left, right, fillValue) {
  * result >=  0  -> index
  */
 function isOneValueInArray(array, value) {
-    var index = array.indexOf(value);
-    if(index == -1) {
-        return -1;
-    }
+  var index = array.indexOf(value);
+  if (index == -1) {
+    return -1;
+  }
 
-    if (index == array.lastIndexOf(value)) {   // Якщо в рядку лише одне значення false
-        return index;
-    } else {
-        return - 2;
-    }
+  if (index == array.lastIndexOf(value)) { // Якщо в рядку лише одне значення false
+    return index;
+  } else {
+    return -2;
+  }
 }
